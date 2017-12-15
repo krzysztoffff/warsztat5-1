e6031c6b3814697db6024cc2814b2a03b6c2d430
rafalcl <rafalclab@gmail.com>
2017-12-15 15:42:20 +0100

    Add BookController

---
+++ src/main/java/pl/coderslab/warsztat5/controller/BookController.java
@@ -0,0 +1,20 @@
+package pl.coderslab.warsztat5.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pl.coderslab.warsztat5.model.Book;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+	@RequestMapping("/hello")
+	public String hello() {
+		return "{hello: World}";
+	}
+
+	@RequestMapping("/helloBook")
+	public Book helloBook() {
+		return new Book(1L, "9788324631766", "Thiniking in Java", "Bruce Eckel", "Helion", "programming");
+	}
+}
